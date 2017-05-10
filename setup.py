878c004f7c1cbdcb101d9ec3a45c32f70717a651
Eduardo Martinez <eduardodsat@hotmail.com>
2017-05-09 23:32:39 -0500

    submodulo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ magicloop-pipeline: Pipeline magicloop
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.org'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
